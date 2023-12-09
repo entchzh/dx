13a5af422354936446258cb46d4e84cbd49f10eb
1top10000 <107670523+1top10000@users.noreply.github.com>
2023-12-09 19:08:54 +0900

    Update f.js

--- f.js
+++ f.js
@@ -1,17 +1,13 @@
-const token = "token github_pat_????????????????????????????????????????";
-let sha0;
+const token = "token github_pat_????????????????????????????????????????????????????????";
 const text = "ㅎㅎ";
 fetch("https://api.github.com/repos/1top10000/dx/contents/db.json", {
     method: "GET",
     headers: {
         Authorization: token
     }
-}).then(response => response.json())
-    .then(t => {
-        sha0 = t.sha;
-    });
-fetch("https://api.github.com/repos/1top10000/dx/contents/db.json",
-    {
+}).then(response => response.json()).then(res => callback(res))
+function callback(res) {
+    fetch("https://api.github.com/repos/1top10000/dx/contents/db.json", {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
@@ -22,11 +18,14 @@ fetch("https://api.github.com/repos/1top10000/dx/contents/db.json",
             repo: 'dx',
             path: 'dx/db.json',
             message: 'my commit message',
-            content: btoa(text),
-            sha: sha0,
+            content: window.btoa(unescape(encodeURIComponent(text))),
+            sha: res.sha,
             committer: {
                 name: '1top10000',
                 email: 'sexesoso.t.tn12345@gmail.com'
             }
         })
-    }).then(() => { });
+    }).then(() => {
+        //
+    });
+}
